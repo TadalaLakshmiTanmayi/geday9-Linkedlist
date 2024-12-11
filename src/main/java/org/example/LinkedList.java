0b7f60eb3bf7c165e684ea565b7f93b4cf575b8c
Tanmayi tadala <tanmayitadala@gmail.com>
2024-12-11 23:06:28 +0530

    UC1 commit

---
+++ src/main/java/org/example/LinkedList.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class LinkedList<T> {
+    private Node<T> head;
+    private int size;
+
+    public LinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public void display() {
+        Node<T> current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public int size() {
+        return size;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.add(56);
+        ll.add(30);
+        ll.add(70);
+        ll.display();
     }
 }
